1064bcf3e559091ac73538a9e990d6cb438d940c
Denilson Mucavele <77127716+Denilson87@users.noreply.github.com>
2022-04-06 12:22:23 +0200

    first realese

---
+++ src/App.jsx
@@ -0,0 +1,107 @@
+import styled from "styled-components";
+import Input from "./components/Input";
+import Button from "./components/Button";
+import Icon from "./components/Icon";
+
+function App() {
+
+  return <MainContainer>
+    
+    <WelcomeText>Contactos</WelcomeText>
+    <InputContainer>
+      <Input type="Text" placeholder="Pesquisar contacto" />
+    </InputContainer>
+  </MainContainer>
+
+}
+
+const MainContainer = styled.div`
+ display: flex;
+ align-items: center;
+ flex-direction: column;
+ height: 80vh;
+ width: 100vw;
+ background: rgba(255, 255, 255, 0.15);
+ box-shadow: 0 8px 32px 0 rgba(31,38,135,0.37);
+ backdrop-filter: blur(8.5px);
+ border-radius: 10px;
+ color:#ffff;
+ text-transform: uppercase;
+ letter-spacing: 0.4rem;
+ @media only screen and (max-width:320px)
+ {
+   width:80vw;
+   height:90vh;
+   hr {
+     margin-bottom: 0.3rem;
+   }
+   h4 {
+     font-size: small;
+     margin-top:-20px;
+   }
+ }
+ @media only screen and (min-width:360px)
+ {
+   width:80vw;
+   height:90vh;
+  
+   h4 {
+     font-size: small;
+    
+   }
+ }
+ @media only screen and (min-width:411px)
+ {
+   width:80vw;
+   height:90vh;
+   }
+
+@media only screen and (min-width:768px)
+ {
+   width:80vw;
+   height:90vh;
+   }
+
+@media only screen and (min-width:1024px)
+ {
+   width:70vw;
+   height:50vh;
+   }
+@media only screen and (min-width:1030px)
+ {
+   width:30vw;
+   height:70vh;
+   }
+
+
+`;
+
+const WelcomeText = styled.h2`
+ 
+  margin: 3rem 0 2rem 0;
+  color:#0c0c0c;
+  
+
+`;
+
+const InputContainer = styled.div`
+
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+align-items: center;
+height: 20%;
+width: 100%;
+
+`;
+
+const IconsContainer = styled.div`
+
+display:flex;
+justify-content: space-evenly;
+margin:2rem 0 3rem 0;
+width: 80%;
+
+`;
+
+export default App;
